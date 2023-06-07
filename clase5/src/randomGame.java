a189fc89b5c0a75bf53acd0f0e22b3298d4fbc50
Jeison Anillo <jeisondejesus@hotmail.es>
2023-06-07 17:03:31 -0500

    solo se puso la condicion del while entrando al 1er if

--- clase5/src/randomGame.java
+++ clase5/src/randomGame.java
@@ -5,15 +5,16 @@
 import java.util.Scanner;
 public class randomGame {
     public static void main(String[] args) {
-        var hiddenNum = 37;
+
         Scanner data = new Scanner(System.in);
         System.out.println("Adivina el numero entre 1 a 100");
         int userData = Integer.parseInt(data.nextLine());
-        while (userData <= 100 && userData >= 0) {
+        var hiddenNum = 37;
+        while (userData <= hiddenNum && userData >= hiddenNum) {
             if (userData == hiddenNum){
                 System.out.println("Felicidades has adivinado el numero!"+hiddenNum);
                 break;
-            } else if ((userData > 30) && (userData < 40)) {
+            } else if (userData > 30 && userData < 40) {
                 System.out.println("Estas cerca de adivinar, sigue intentando");
             }else if ((userData > 33) && (userData < -39)) {
                 System.out.println("Estas cerca, sigue intentando");
