c3e0680d35ccac8cfbce368030421ca82221af8a
Jeison Anillo <jeisondejesushotmail.es>
2023-06-13 03:39:55 -0500

    Execise resolved!

--- clase5/src/randomGame.java
+++ clase5/src/randomGame.java
@@ -5,22 +5,21 @@
 import java.util.Scanner;
 public class randomGame {
     public static void main(String[] args) {
-        var hiddenNum = 37;
+        var hiddenNum = (int)(Math.random() * 50)+1;
         Scanner data = new Scanner(System.in);
         System.out.println("Adivina el numero entre 1 a 100");
         int userData = Integer.parseInt(data.nextLine());
-        while (userData <= 100 && userData >= 0) {
-            if (userData == hiddenNum){
-                System.out.println("Felicidades has adivinado el numero!"+hiddenNum);
+        while (true) {
+            if (userData == hiddenNum) {
+                System.out.println("Felicidades has adivinado el numero! " + hiddenNum);
                 break;
-            } else if ((userData > 30) && (userData < 40)) {
-                System.out.println("Estas cerca de adivinar, sigue intentando");
-            }else if ((userData > 33) && (userData < -39)) {
-                System.out.println("Estas cerca, sigue intentando");
-            }else if((userData < -0) || (userData > 100)){
-                System.out.println("Estas fuera del rango indicado :/");
-            }else System.out.println("Sigue intentando!");
-            userData++;
+            } else if (userData <= 0 || userData > 100) {
+                System.out.println("Esto es increible!!!, no sigues instrucciones!!! :/");
+                break;
+            }else{
+                System.out.println("Sigue intentando!");
+                userData = Integer.parseInt(data.nextLine());
+            }
         }
     }
 }
